877a274ab3c46382c73f87462b90b1675e7b9dff
WillJCJ <willjcjohnson@gmail.com>
2015-04-18 17:12:07 +0100

    Almost there
    bit weird though

---
+++ hello/www/info/killhopeInfo.js
@@ -0,0 +1,28 @@
+var kill_info = [
+	{
+		"id":"contact",
+		"title":"Contact Details",
+		"text":[
+			"Telephone:\n01388 537 505",
+			"Email:\ninfo@killhope.org.uk",
+			"Address:\nKillhope, The North of England Lead Mining Museum\nNear Cowshill\nUpper Weardale\nCounty Durham\nDL13 1AR"
+		]
+	},
+	{
+		"id":"about",
+		"title":"About Killhope",
+		"text":[
+			"Killhope is a mine and it's really good.",
+			"You should go some time."
+		]
+	},
+	{
+		"id":"history",
+		"title":"History of Killhope",
+		"text":[
+			"Killhope was a mine now it's a really good museum.",
+			"You should go some time."
+		]
+	},
+
+];
