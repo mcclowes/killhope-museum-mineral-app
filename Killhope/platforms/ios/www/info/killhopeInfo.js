0f56eecc748983a6f6d1293bc99afa8105260e4d
Max Clayton Clowes <maxcc@me.com>
2015-05-07 15:03:48 +0100

    Splash/Icons

---
+++ Killhope/platforms/ios/www/info/killhopeInfo.js
@@ -0,0 +1,29 @@
+var info = [
+	{
+		"id":"contact",
+		"title":"Contact Details",
+		"text":[
+			"Telephone: 01388 537 505",
+			"Email: info@killhope.org.uk",
+			"Address:<br>Killhope, The North of England Lead Mining Museum<br>Near Cowshill<br>Upper Weardale<br>County Durham<br>DL13 1AR"
+		]
+	},
+	{
+		"id":"about",
+		"title":"About Killhope",
+		"text":[
+			"The North of England Lead Mining Museum, also known as Killhope, is an industrial museum near the village of Cowshill, County Durham, England. The museum stands on the site of the former Park Level Mine, which is being restored to show the workings of a 19th-century lead mine.",
+			"The museum is open every day between 1 April and 31 October, but is closed (except to pre-booked groups) during the winter months, when weather conditions in the area can be severe. A bus service operates through Weardale; services usually terminate at Cowshill, but some summer services will continue to Killhope on request."
+		]
+	},
+	{
+		"id":"history",
+		"title":"History",
+		"text":[
+			"From 1818, mining in the area was controlled by W B Lead Co, a mining company established by the Blacketts, a prominent Newcastle family which had leased mining rights in Weardale from the Bishop of Durham.",
+			"In 1853, W B Lead began driving the Park Level Mine, which eventually intersected 11 mineral veins. As the mine developed, so also did the surface workings. In 1858 a \"mineshop\" was built to accommodate the miners.",
+			"After the First World War, it lay derelict for over 60 years, during which time the buildings crumbled and any equipment that could be removed was salvaged for scrap.",
+			"By 1980, the Killhope Wheel was facing demolition, the washing floor had become a marshy field, and the rest of the site was decaying. In that year, Durham County Council took over the site and began a programme of restoration. First to be restored was the \"mineshop\", which was opened to the public in May 1984. The Killhope Wheel was restored to working order in 1991 and the mine itself was opened in 1996."
+		]
+	}
+];
